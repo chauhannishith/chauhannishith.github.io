768eb076e310040e3a202ec9fe1db85944a42c66
chauhannishith <chauhannishith94@gmail.com>
2026-03-07 12:43:31 +0530

    make experience modular and easy to update

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -23,7 +23,7 @@ export const Section = ({children, id, title, dark}: {children: React.ReactNode,
           },
         }}>
           {title && (
-            <Typography variant='h2' fontWeight={600}  style={{
+            <Typography variant='h2' fontWeight={600} style={{
               marginTop: '0'
             }}>
               {title}
